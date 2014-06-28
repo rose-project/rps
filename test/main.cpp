ec71f640dfc101cd4c6a3e44d716c1c4a33866b3
Josef Raschen <josef@raschen.org>
2014-06-28 16:57:47 +0200

    Repair unit tests.

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,5 @@
+#include <stdlib.h>
+#include <sys/stat.h>
 #include "mpk/mpk.h"
 #include "gtest/gtest.h"
 
@@ -8,8 +10,15 @@ TEST(MpkHighLevelApi, package_create) {
         mpk_create(TESTDATA_DIR "testpackage","/tmp",
         TESTDATA_DIR "privkey.pem"));
     mpk_deinit();
+
+    struct stat st;
+    EXPECT_EQ(0,  stat("/tmp/testpackage.mpk", &st));
+    unlink("/tmp/testpackage.mpk");
 }
 
-TEST(MpkHighLevelApi, package_install) {
-    EXPECT_EQ(MPK_SUCCESS, mpk_install(TESTDATA_DIR"/testpackage.mpk"));
+TEST(MpkHighLevelApi, package_unpack) {
+    EXPECT_EQ(MPK_SUCCESS, mpk_init());
+    EXPECT_EQ(MPK_SUCCESS, mpk_unpack(TESTDATA_DIR"/testpackage.mpk", "/tmp"));
+    mpk_deinit();
+    system("rm -rf /tmp/testpackage");
 }
