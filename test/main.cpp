b08d47d5bca1cd0fac593dd42dba95668e2bc53b
Josef Raschen <josef@raschen.org>
2016-01-05 14:08:53 +0100

    Remove googletest.

--- test/main.cpp
+++ test/main.cpp
@@ -1,25 +1,8 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include "mpk/mpk.h"
-#include "gtest/gtest.h"
 
-TEST(MpkHighLevelApi, package_create) {
-
-    EXPECT_EQ(MPK_SUCCESS, mpk_init());
-    EXPECT_EQ(MPK_SUCCESS,
-        mpk_create(TESTDATA_DIR "testpackage","/tmp",
-        TESTDATA_DIR "privkey.pem"));
-    mpk_deinit();
-
-    struct stat st;
-    EXPECT_EQ(0,  stat("/tmp/testpackage.mpk", &st));
-    unlink("/tmp/testpackage.mpk");
-}
-
-TEST(MpkHighLevelApi, package_unpack) {
-    EXPECT_EQ(MPK_SUCCESS, mpk_init());
-    EXPECT_EQ(MPK_SUCCESS, mpk_unpack(TESTDATA_DIR
-        "/testpackage-1.2.0-R2013031915000000.mpk", "/tmp"));
-    mpk_deinit();
-    system("rm -rf /tmp/testpackage");
+int main()
+{
+    return 0;
 }
